53e09a75c0cd5baa9a067e724ea90b76ad4a435c
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-14 12:19:49 +0530

    question

--- src/Lecture4/farenheit_to_celsius.java
+++ src/Lecture4/farenheit_to_celsius.java
@@ -39,5 +39,25 @@ Sample Output 2:
 Explanation For Input 2:
 We need need to start calculating the Celsius values for each of the Fahrenheit Value which starts from 20. So starting from 20 which is the given Fahrenheit start value, we need to compute its corresponding Celsius value which computes to -6. We print this information as <Fahrenheit Value> a tab space"\t" <Celsius Value> on each line for each step of 13 we take to get the next value of Fahrenheit and extend this idea till we reach the end that is till 119 in this case. You may or may not exactly land on the end value depending on the steps you are taking.
  */
+import java.util.Scanner;
 public class farenheit_to_celsius {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int S = s.nextInt();
+        int E = s.nextInt();
+        int W = s.nextInt();
+        if(S>= 0 && S<=80){
+            if(E>=S && E<=900){
+                if(W>=0 && W<=40){
+                    for(int i=S; i<=E; i=i+W){
+                        int C = (i-32)*5/9;
+                        System.out.println(i+"\t"+C);
+                    }
+                }
+                else System.out.println("Enter W value between 0 and 40");
+            }
+            else System.out.println("Enter E value between "+S+" and 900 ");
+        }
+        else System.out.println("Enter S value between 0 and 80");
+    }
 }
