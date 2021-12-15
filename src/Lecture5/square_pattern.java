34a55d18c46dd302865d1795d43fab147aef8f2e
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-15 17:21:50 +0530

    question

--- src/Lecture5/square_pattern.java
+++ src/Lecture5/square_pattern.java
@@ -34,5 +34,16 @@ Sample Output 1:
 666666
 666666
  */
+import java.util.Scanner;
 public class square_pattern {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n= s.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
 }
