732bed8da126e938627bd4e731ee6ff3d4635a53
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-15 17:45:03 +0530

    question

--- src/Lecture5/triangle_pattern.java
+++ src/Lecture5/triangle_pattern.java
@@ -20,18 +20,29 @@ Sample Input 1:
 Sample Output 1:
 *
 **
-*
-**
 ***
+*****
+******
 Sample Input 2:
 6
 Sample Output 2:
 *
 **
-*
-**
 ***
-**
+****
+*****
+******
  */
+import java.util.Scanner;
 public class triangle_pattern {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0; i<n;i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
