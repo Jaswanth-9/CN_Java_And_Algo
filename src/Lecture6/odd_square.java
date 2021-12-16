7c30a06dadb7f4a45f712e59158a086a44abdb17
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-16 16:56:04 +0530

    question

--- src/Lecture6/odd_square.java
+++ src/Lecture6/odd_square.java
@@ -29,5 +29,23 @@ Sample Output 2 :
 79135
 91357
  */
+import java.util.Scanner;
 public class odd_square {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0;i<n;i++){
+            int l=0;
+            int m=0;
+            for(int j=i;j<n;j++){
+                System.out.print(1+(2*i)+(2*l));
+                l++;
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(k+m);
+                m++;
+            }
+            System.out.println();
+        }
+    }
 }
