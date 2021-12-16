68227cb45a29388827362aec9f0679bd5440727e
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-16 16:26:57 +0530

    question

--- src/Lecture6/parallelogram_pattern.java
+++ src/Lecture6/parallelogram_pattern.java
@@ -28,5 +28,19 @@ Sample Output 2 :
    ***
     ***
  */
+import java.util.Scanner;
 public class parallelogram_pattern {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=2;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(n-2);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
