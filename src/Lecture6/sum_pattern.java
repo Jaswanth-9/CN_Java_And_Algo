3e7153ca35eeed7b79936f32548955f787e0326e
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-16 18:08:02 +0530

    question

--- src/Lecture6/sum_pattern.java
+++ src/Lecture6/sum_pattern.java
@@ -24,5 +24,22 @@ Sample Output 2 :
 1+2+3+4=10
 1+2+3+4+5=15
  */
+import java.util.Scanner;
 public class sum_pattern {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=1;i<=n;i++){
+            int m = 0;
+            for(int j=1;j<=i;j++){
+                if(i==j){
+                    m += j;
+                    System.out.print(j+"="+m);
+                }
+                else System.out.print(j+"+");
+                m += j;
+            }
+            System.out.println();
+        }
+    }
 }
