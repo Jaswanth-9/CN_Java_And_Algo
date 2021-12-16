93bc0c38c2aa2ec238c538b5dbe2fe982646d55f
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-16 19:07:10 +0530

    question

--- src/Lecture6/half_diamond.java
+++ src/Lecture6/half_diamond.java
@@ -36,5 +36,28 @@ Sample Output 2 :
 1
 *
  */
+import java.util.Scanner;
 public class half_diamond {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int k=i-1;k>0;k--){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+        for(int i=n-1; i>0; i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int k=i-1;k>0;k--){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
 }
