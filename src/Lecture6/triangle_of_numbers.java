743113ace8b3f6055945fcd24aaf2b2c1c1e96b9
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-16 17:51:29 +0530

    question

--- src/Lecture6/triangle_of_numbers.java
+++ src/Lecture6/triangle_of_numbers.java
@@ -31,5 +31,26 @@ Sample Output 2:
       34543
      4567654
  */
+import java.util.Scanner;
 public class triangle_of_numbers {
+    public static void main(String[] rgs){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int x =0;
+        for(int i=1;i<=n;i++){
+            int g = 0;
+            for(int j=n-1;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=i; k<(2*i); k++){
+                System.out.print(k);
+            }
+            for(int m=i+2;m<(2*i)+1;m++) {
+                System.out.print(2*x-g);
+                g++;
+            }
+            x++;
+            System.out.println();
+        }
+    }
 }
