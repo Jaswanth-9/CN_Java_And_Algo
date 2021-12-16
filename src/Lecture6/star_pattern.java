d8ea040100501312efcfcb7f224c5b3bd1231fd3
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-16 16:18:07 +0530

    question

--- src/Lecture6/star_pattern.java
+++ src/Lecture6/star_pattern.java
@@ -29,5 +29,20 @@ Sample Output 2 :
   ***
  ****
  */
+import java.util.Scanner;
 public class star_pattern {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=n-i;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
