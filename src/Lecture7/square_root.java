86e8873329b6d60a3315f9c668790d02296740a8
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-19 11:15:32 +0530

    question

--- src/Lecture7/square_root.java
+++ src/Lecture7/square_root.java
@@ -19,5 +19,16 @@ Sample Input 2 :
 Sample Output 2 :
 2
  */
+import java.util.Scanner;
 public class square_root {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i =1;i<(n/2);i++){
+            if((i*i)>n){
+                System.out.println((i-1)*(i-1));
+                System.exit(-1);
+            }
+        }
+    }
 }
