2d816aa8185217e2da92ee7f4971f78bdb2d360b
Jaswanth-9 <jaswanthkumars1999@gmail.com>
2021-12-21 16:35:13 +0530

    question

--- src/Lecture8/assign_array_intersection.java
+++ src/Lecture8/assign_array_intersection.java
@@ -60,7 +60,6 @@ public class assign_array_intersection {
         ArrayList<Integer> a1 = new ArrayList<>();
         ArrayList<Integer> a2 = new ArrayList<>();
         ArrayList<Integer> a3 = new ArrayList<>();
-        int c = 0;
         for(int i=1; i<=t; i++){
             int n1 = s.nextInt();
             for(int j=0; j<n1; j++){
@@ -75,9 +74,8 @@ public class assign_array_intersection {
             for(int x=0; x<a1.size(); x++){
                 for(int y=0; y<a2.size(); y++){
                     if(a1.get(x) == a2.get(y)){
-                        a3.add(c);
+                        a3.add(a2.get(y));
                         a2.remove(y);
-                        c++;
                     }
                 }
             }
